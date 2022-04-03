17d6bc08e01a3ef22a9f1ea22232da0c9ee964a8
Kevin Amel Rosario Cruz <58832812+kevinrosarioc@users.noreply.github.com>
2022-04-03 15:19:29 -0400

    added row options for tables

--- src/pages/CustomersPage.js
+++ src/pages/CustomersPage.js
@@ -1,113 +1,10 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Table from "../components/Table/Table";
 import Layout from "../components/Layout/Layout";
+import RowOptions from "../components/Table/RowOptions";
+import spinner from "../assets/Images/spinner.gif";
 
 const Customers = () => {
-  const data = useMemo(
-    () => [
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      },
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      },
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      },
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      },
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      },
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      },
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      },
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      },
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      },
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      },
-      {
-        number: "1",
-        id: "3FGS2",
-        date: "Marzo 31, 2022",
-        address: "Juan Isidro Ortega",
-        email: "kev@gmail.com",
-        phone: "8092321180",
-        options: ""
-      }
-    ],
-    []
-  );
-
   const columns = useMemo(
     () => [
       {
@@ -117,6 +14,10 @@ const Customers = () => {
       {
         Header: "Código",
         accessor: "id"
+      },
+      {
+        Header: "Nombre",
+        accessor: "name"
       },
       {
         Header: "Fecha registro",
@@ -141,19 +42,65 @@ const Customers = () => {
     ],
     []
   );
+
+  const [tableData, setTableData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const mapClientsData = (clientsData) => {
+    return clientsData.map((client, index) => ({
+      number: index + 1,
+      id: client.id,
+      name: `${client.names} ${client.lastnames}`,
+      date: new Date(client.created).toLocaleDateString("es-DO"),
+      address: `${client.address1.substring(0, 16)}...`,
+      email: client.email,
+      phone: client.phone,
+      options: (
+        <>
+          <RowOptions />
+        </>
+      )
+    }));
+  };
+  useEffect(async () => {
+    setIsLoading(true);
+    try {
+      // eslint-disable-next-line no-undef
+      const response = await fetch(`${process.env.REACT_APP_NOTIPET_API_URL}/users`);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        const filteredClients = jsonResponse.filter((user) => user.role === 0); // client role is 0
+        const clients = mapClientsData(filteredClients);
+        setTableData(clients);
+      } else {
+        throw new Error(
+          `Error ${response.status}: Algo salió mal al intentar recuperar los clientes`
+        );
+      }
+    } catch (error) {
+      setError({ message: error.message });
+    }
+    setIsLoading(false);
+  }, []);
+
   return (
     <Layout>
       <div>
         <h2 className="page-header">Clientes</h2>
-        <div className="row">
-          <div className="col-12">
-            <div className="card">
-              <div className="card__body">
-                <Table columns={columns} data={data} />
+        {error && <p>{error.message}</p>}
+        {isLoading && <img src={spinner} alt="" width="40" height="40" />}
+        {!error && (
+          <div className="row">
+            <div className="col-12">
+              <div className="card">
+                <div className="card__body">
+                  <Table columns={columns} data={tableData} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </Layout>
   );
