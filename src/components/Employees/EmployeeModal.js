a24aadc1386364fc88aa5096b5d6ebe31a72c502
Kevin Amel Rosario Cruz <58832812+kevinrosarioc@users.noreply.github.com>
2022-04-15 19:17:04 -0400

    edit/details employees

---
+++ src/components/Employees/EmployeeModal.js
@@ -0,0 +1,154 @@
+import { useState, useEffect } from "react";
+import Modal from "../ui/Modal/Modal";
+import Button from "../ui/Button/Button";
+import { useForm } from "react-hook-form";
+import Input from "../ui/Input/Input";
+import classes from "./EmployeeModal.module.css";
+import spinner from "../../assets/Images/spinner.gif";
+
+// eslint-disable-next-line no-unused-vars
+const EmployeeModal = ({ canEdit, username, onClose, refreshTable }) => {
+  const { register, handleSubmit, setValue } = useForm();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [initialEmployeeData, setInitialEmployeeData] = useState({});
+  const [editSuccess, setEditSuccess] = useState(null);
+  // const [employeeIsActive, setEmployeeIsActive] = useState(false);
+
+  const fillInitialForm = (employee) => {
+    setValue("id", employee.id);
+    setValue("document", employee.document);
+    setValue("names", employee.names);
+    setValue("lastnames", employee.lastnames);
+    setValue("username", employee.username);
+    setValue("role", +employee.role);
+    setValue("phone", employee.phone);
+    setValue("address1", employee.address1);
+    setValue("address2", employee.address2);
+    setValue("city", employee.city);
+    setValue("province", employee.province);
+    setValue("created", new Date(employee.created).toLocaleDateString("es-DO"));
+    setValue("updated", new Date(employee.updated).toLocaleDateString("es-DO"));
+  };
+
+  const fetchEmployee = async () => {
+    setIsLoading(true);
+    try {
+      // eslint-disable-next-line no-undef
+      const response = await fetch(`${process.env.REACT_APP_NOTIPET_API_URL}/users/${username}`);
+      if (response.ok) {
+        const { data: employee } = await response.json();
+        setInitialEmployeeData(employee);
+        fillInitialForm(employee);
+      } else {
+        throw new Error(
+          `Error ${response.status}: Algo salió mal al intentar recuperar los datos del empleado`
+        );
+      }
+    } catch (error) {
+      setError({ message: error.message });
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(async () => {
+    fetchEmployee();
+  }, []);
+
+  const formSubmitHandler = async (data) => {
+    // eslint-disable-next-line no-undef
+    const endpoint = `${process.env.REACT_APP_NOTIPET_API_URL}/users/userId?userId=${data.id}`;
+    const method = "PUT";
+    const reqHeaders = { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" };
+    const reqBody = JSON.stringify({
+      ...initialEmployeeData,
+      ...data,
+      role: +data.role
+    });
+    setIsLoading(true);
+    setEditSuccess(false);
+    setError(null);
+    try {
+      const response = await fetch(endpoint, {
+        method: method,
+        body: reqBody,
+        headers: reqHeaders
+      });
+      if (response.ok) {
+        setEditSuccess(true);
+        fetchEmployee();
+        refreshTable();
+      } else {
+        throw new Error(`${response.status}: Algo salió mal al editar el empleado`);
+      }
+    } catch (error) {
+      setError({ message: error.message });
+    }
+    setIsLoading(false);
+  };
+  return (
+    <Modal onClick={onClose}>
+      <header>
+        <h2>{canEdit ? "Editar empleado" : "Detalles del empleado"}</h2>
+      </header>
+      <main>
+        {editSuccess && <p>Editado exitosamente</p>}
+        {error && <p>{error.message}</p>}
+        {isLoading && <img src={spinner} alt="" width="40" height="40" />}
+        {!isLoading && (
+          <form onSubmit={handleSubmit(formSubmitHandler)} className={classes.form}>
+            <Input label="Código" input={{ ...register("id"), disabled: true }} />
+            <Input
+              label="Documento"
+              input={{ ...register("document"), disabled: !canEdit }} // TODO: function to set doc type
+            />
+            <Input label="Nombres" input={{ ...register("names"), disabled: !canEdit }} />
+            <Input label="Apellidos" input={{ ...register("lastnames"), disabled: !canEdit }} />
+            <Input label="Nombre de usuario" input={{ ...register("username"), disabled: true }} />
+            <label htmlFor="type" className={classes["select-label"]}>
+              Rol
+            </label>
+            <select {...register("role")} className={classes.select} id="role" disabled={!canEdit}>
+              <option value="1">Representante de ventas</option>
+              <option value="2">Administrador</option>
+              <option value="3">Especialista</option>
+            </select>
+            <Input label="Teléfono" input={{ ...register("phone"), disabled: !canEdit }} />
+            <Input label="Dirección 1" input={{ ...register("address1"), disabled: !canEdit }} />
+            <Input label="Dirección 2" input={{ ...register("address2"), disabled: !canEdit }} />
+            <Input label="Ciudad" input={{ ...register("city"), disabled: !canEdit }} />
+            <Input label="Provincia" input={{ ...register("province"), disabled: !canEdit }} />
+            <Input
+              label="Fecha de creación"
+              input={{
+                ...register("created"),
+                disabled: true
+              }}
+            />
+            <Input
+              label="Última actualización"
+              input={{
+                ...register("updated"),
+                disabled: true
+              }}
+            />
+            <div className={classes.actions}>
+              {
+                <Button type="button" onClick={onClose} isPrimary={false}>
+                  Cerrar
+                </Button>
+              }
+              {canEdit && (
+                <Button type="submit" isPrimary isLoading={isLoading} disabled={isLoading}>
+                  Editar
+                </Button>
+              )}
+            </div>
+          </form>
+        )}
+      </main>
+    </Modal>
+  );
+};
+
+export default EmployeeModal;
--- src/pages/EmployeesPage.js
+++ src/pages/EmployeesPage.js
@@ -1,56 +1,177 @@
-import React from "react";
+import React, { useEffect, useMemo, useState, useContext } from "react";
+import UserContext from "../store/user-context";
 import Table from "../components/Table/Table";
 import Layout from "../components/Layout/Layout";
-import customerList from "../assets/JsonData/customers-list.json";
-
-const customerTableHead = [
-  "#",
-  "Código",
-  "Cliente",
-  "Fecha registro",
-  "Dirección",
-  "Email",
-  "Teléfono",
-  "Monto",
-  "Opciones"
-];
-
-const renderHead = (item, index) => <th key={index}>{item}</th>;
-
-const renderBody = (item, index) => (
-  <tr key={index}>
-    <td>{item.id}</td>
-    <td>{item.customerCode}</td>
-    <td>{item.name}</td>
-    <td>{item.registrationDate}</td>
-    <td>{item.location}</td>
-    <td>{item.email}</td>
-    <td>{item.phone}</td>
-    <td>{item.amount}</td>
-  </tr>
-);
+import RowOptions from "../components/Table/RowOptions";
+import spinner from "../assets/Images/spinner.gif";
+import EmployeeModal from "../components/Employees/EmployeeModal";
 
 const Employees = () => {
+  const columns = useMemo(
+    () => [
+      {
+        Header: "#",
+        accessor: "number"
+      },
+      {
+        Header: "Código",
+        accessor: "id"
+      },
+      {
+        Header: "Nombre",
+        accessor: "name"
+      },
+      {
+        Header: "Fecha registro",
+        accessor: "date"
+      },
+      {
+        Header: "Rol",
+        accessor: "role"
+      },
+      {
+        Header: "Dirección",
+        accessor: "address"
+      },
+      {
+        Header: "Email",
+        accessor: "email"
+      },
+      {
+        Header: "Teléfono",
+        accessor: "phone"
+      },
+      {
+        Header: "Opciones",
+        accessor: "options"
+      }
+    ],
+    []
+  );
+
+  const userContext = useContext(UserContext);
+  const [tableData, setTableData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [showEditEmployee, setShowEditEmployee] = useState(false);
+  const [showEmployeeDetails, setShowEmployeeDetails] = useState(false);
+  const [currentEmployeeUsername, setCurrentEmployeeUsername] = useState(null);
+
+  const editEmployeeHandler = (username) => {
+    setCurrentEmployeeUsername(username);
+    setShowEditEmployee(true);
+  };
+
+  const viewEmployeeDetailsHandler = (username) => {
+    setCurrentEmployeeUsername(username);
+    setShowEmployeeDetails(true);
+  };
+
+  const closeModalHandler = () => {
+    setShowEmployeeDetails(false);
+    setShowEditEmployee(false);
+  };
+
+  const mapEmployeesData = (employeesData) => {
+    return employeesData.map((employee, index) => {
+      let role;
+      switch (employee.role) {
+        case 1:
+          role = "Representante de ventas";
+          break;
+        case 2:
+          role = "Administrador";
+          break;
+        case 3:
+          role = "Especialista";
+          break;
+        default:
+          role = "Otro";
+          break;
+      }
+      return {
+        number: index + 1,
+        id: employee.id,
+        username: employee.username,
+        role,
+        name: `${employee.names} ${employee.lastnames}`,
+        date: new Date(employee.created).toLocaleDateString("es-DO"),
+        address: `${employee.address1.substring(0, 16)}...`,
+        email: employee.email,
+        phone: employee.phone,
+        options: (
+          <>
+            <RowOptions
+              onEdit={() => editEmployeeHandler(employee.username)}
+              onViewDetails={() => viewEmployeeDetailsHandler(employee.username)}
+            />
+          </>
+        )
+      };
+    });
+  };
+
+  const fetchEmployees = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        // eslint-disable-next-line no-undef
+        `${process.env.REACT_APP_NOTIPET_API_URL}/users/bybusiness/${userContext.businessId}`
+      );
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        const currentBusinessEmployees = jsonResponse.data.filter((user) => +user.role !== 0);
+        const employees = mapEmployeesData(currentBusinessEmployees);
+        setTableData(employees);
+      } else {
+        throw new Error(
+          `Error ${response.status}: Algo salió mal al intentar recuperar los empleados`
+        );
+      }
+    } catch (error) {
+      setError({ message: error.message });
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(async () => {
+    fetchEmployees();
+  }, []);
+
   return (
     <Layout>
       <div>
         <h2 className="page-header">Empleados</h2>
-        <div className="row">
-          <div className="col-12">
-            <div className="card">
-              <div className="card__body">
-                <Table
-                  limit="10"
-                  headData={customerTableHead}
-                  renderHead={(item, index) => renderHead(item, index)}
-                  bodyData={customerList}
-                  renderBody={(item, index) => renderBody(item, index)}
-                />
+        {error && <p>{error.message}</p>}
+        {isLoading && <img src={spinner} alt="" width="40" height="40" />}
+        {tableData.length === 0 && !isLoading && <p>No se encontraron empleados</p>}
+        {!error && tableData.length > 0 && (
+          <div className="row">
+            <div className="col-12">
+              <div className="card">
+                <div className="card__body">
+                  <Table columns={columns} data={tableData} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
+      {showEditEmployee && (
+        <EmployeeModal
+          canEdit
+          username={currentEmployeeUsername}
+          onClose={closeModalHandler}
+          refreshTable={fetchEmployees}
+        />
+      )}
+      {showEmployeeDetails && (
+        <EmployeeModal
+          canEdit={false}
+          username={currentEmployeeUsername}
+          onClose={closeModalHandler}
+        />
+      )}
     </Layout>
   );
 };
